8c1a4c098b7f46ed2a0c283378116297a9dbfda5
DerWikinger <32542782+DerWikinger@users.noreply.github.com>
2018-04-26 09:35:33 +0300

    Some changes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,1 @@
-var con = new ActiveXObject("ADODB.Connection", {
-    activate: false, // Allow activate existance object instance, false by default
-    async: true, // Allow asynchronius calls, true by default (for future usage)
-    type: true	// Allow using type information, true by default
-});
-
-console.dir(con);
+var express = require('express');
